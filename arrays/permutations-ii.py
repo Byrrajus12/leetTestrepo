98705b2c1bd37c384fd99ad50598ae59f5d37161
Sai Byrraju <119342053+Byrrajus12@users.noreply.github.com>
2026-04-09 19:10:34 -0400

    Add: Permutations II [Arrays]

---
+++ arrays/permutations-ii.py
@@ -0,0 +1,34 @@
+# Problem: Permutations II
+# Difficulty: Medium
+# Topics: Array, Backtracking, Sorting
+# URL: https://leetcode.com/problems/permutations-ii/
+# Submitted At: April 9, 2026 7:10 PM
+
+class Solution:
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        used = [False] * len(nums)
+
+        def backtrack(path):
+            if len(path) == len(nums):
+                res.append(path[:])
+                return
+
+            for i in range(len(nums)):
+                if used[i]:
+                    continue
+
+                if i > 0 and nums[i] == nums[i - 1] and not used[i - 1]:
+                    continue
+
+                used[i] = True
+                path.append(nums[i])
+
+                backtrack(path)
+
+                path.pop()
+                used[i] = False
+
+        backtrack([])
+        return res
